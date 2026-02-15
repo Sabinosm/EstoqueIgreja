0fe63a409c1102a804e58d3b7844d2a4b6b10685
Pedro Sabino <sabinop049@gmail.com>
2026-02-14 21:52:36 -0300

    Modify QrCode constructor to call GerarQrCode without URL
    Updated GerarQrCode method call to use a default URL.

--- estoqueIgreja/MineCode/QrCode.cs
+++ estoqueIgreja/MineCode/QrCode.cs
@@ -17,7 +17,7 @@ namespace estoqueIgreja
         public QrCode()
         {
             InitializeComponent();
-            GerarQrCode("https://onedrive.live.com/:x:/g/personal/B1504845243E51DC/IQC6FT95Ole1R4EcAED0wjlKAVjUD9t0xB_IYl76ox-TSrE?resid=B1504845243E51DC!s793f15ba573a47b5811c0040f4c2394a&ithint=file%2Cxlsx&e=cymKRm&migratedtospo=true&redeem=aHR0cHM6Ly8xZHJ2Lm1zL3gvYy9iMTUwNDg0NTI0M2U1MWRjL0lRQzZGVDk1T2xlMVI0RWNBRUQwd2psS0FWalVEOXQweEJfSVlsNzZveC1UU3JFP2U9Y3ltS1Jt");
+            GerarQrCode(); //O link do site ficaria aqui
            
         }
 
